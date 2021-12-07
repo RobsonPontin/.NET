1296c1577136e712ee02790d188973277c4c5d6c
Robson Pontin <66638512+pontin-pdftron@users.noreply.github.com>
2021-12-07 08:43:04 -0800

    Added .NET 6 console app sample project

---
+++ Multi-target PDFNet/PDFNetC64/README.md
@@ -0,0 +1,3 @@
+This folder contains the .NET libraries from the `stable 9.1 nightly build of PDFNetC64 from November 24th 2021`.
+
+To download the latest stable nightly: https://www.pdftron.com/nightly/#stable
---
+++ Multi-target PDFNet/PDFTronPOCNet6ConsoleApp/ProgramNet6.cs
@@ -0,0 +1,17 @@
+﻿using PDFTronCommonLibNet5NetStandard;
+using System;
+
+namespace PDFTronPOCNet6ConsoleApp
+{
+    class ProgramNet6
+    {
+        static void Main(string[] args)
+        {
+
+            PDFTronProvider.initialize();
+            PDFTronProvider.TestPDFs();
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
